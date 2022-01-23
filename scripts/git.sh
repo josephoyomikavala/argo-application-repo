3e9c5bd0198f256c544fc268db466bb142c03fa5
Joseph Oyomi <joseph.oyomi@kevala.com>
2022-01-23 13:55:32 -0600

    updated tag

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,7 +6,8 @@ git config --global user.email "joseph.oyomi@kevala.com"
 cd manifest-repo/templates
 envsubst < application-template.yaml > ../application.yaml
 cd ..
+git remote set-url origin https://josephoyomikavala:$github_password@github.com/josephoyomikavala/manifest-repo.git
 git add -A
-git commit -m 'batmans commit' && git push https://github.com/josephoyomikavala/manifest-repo.git main
+git commit -m 'tag commit' && git push https://github.com/josephoyomikavala/manifest-repo.git main
 
 # git push 'https://source.developers.google.com/p/$PROJECT_ID/r/my-repo' master
