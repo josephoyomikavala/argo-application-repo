0b0e7941c1e1bd69010b3f466751b3362a3fb68b
Joseph Oyomi <joseph.oyomi@kevala.com>
2022-01-23 16:15:58 -0600

    github token

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,4 +1,5 @@
  #!/bin/bash
+echo $github_access_token
 apt-get update && apt-get -y install gettext
 git clone https://github.com/josephoyomikavala/manifest-repo.git
 git config --global user.name josephoyomikavala
@@ -6,7 +7,6 @@ git config --global user.email "joseph.oyomi@kevala.com"
 cd manifest-repo/templates
 envsubst < application-template.yaml > ../application.yaml
 cd ..
-echo $github_access_token
 git config remote.origin.url 'https://${github_access_token}@github.com/josephoyomikavala/manifest-repo.git'
 git add -A
 git commit -m 'tag commit' && git push
