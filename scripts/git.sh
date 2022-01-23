ab55f93379d191275089e7957bfff8c2b3bbac1f
Joseph Oyomi <joseph.oyomi@kevala.com>
2022-01-23 17:34:00 -0600

    edited tag

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,7 +5,7 @@ git clone https://github.com/josephoyomikavala/manifest-repo.git
 git config --global user.name josephoyomikavala
 git config --global user.email "joseph.oyomi@kevala.com"
 cd manifest-repo/templates
-envsubst < application-template.yaml > ../application.yaml
+envsubst < application-template.yaml > ../source/application.yaml
 cd ..
 git config remote.origin.url "https://$github_access_token@github.com/josephoyomikavala/manifest-repo.git"
 git add -A
