5dd6f5bac407c81e7531a1b076752e84c200db9f
Joseph Oyomi <joseph.oyomi@kevala.com>
2022-01-23 16:21:44 -0600

    github token

--- scripts/git.sh
+++ scripts/git.sh
@@ -7,7 +7,7 @@ git config --global user.email "joseph.oyomi@kevala.com"
 cd manifest-repo/templates
 envsubst < application-template.yaml > ../application.yaml
 cd ..
-git config remote.origin.url 'https://$(echo $github_access_token)@github.com/josephoyomikavala/manifest-repo.git'
+git config remote.origin.url "https://$github_access_token@github.com/josephoyomikavala/manifest-repo.git"
 git add -A
 git commit -m 'tag commit' && git push
 
