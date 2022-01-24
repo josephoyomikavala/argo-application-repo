547cfbc988e20f7a4acb135d15d271dc43ce8ce2
Joseph Oyomi <joseph.oyomi@kevala.com>
2022-01-24 05:27:47 -0600

    added image set

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,5 +1,4 @@
  #!/bin/bash
-echo $github_access_token
 apt-get update && apt-get -y install gettext
 git clone https://github.com/josephoyomikavala/manifest-repo.git
 git config --global user.name josephoyomikavala
@@ -8,6 +7,6 @@ cd manifest-repo/templates
 envsubst < application-template.yaml > ../source/application.yaml
 cd ..
 git config remote.origin.url "https://$github_access_token@github.com/josephoyomikavala/manifest-repo.git"
-git add -A
+git add .
 git commit -m 'tag commit' && git push
 
